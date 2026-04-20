f52dff0eb1172f41a18d69e26074d64a87ecba6e
Selim Celem <selim.celem@gmail.com>
2026-04-20 16:35:20 +0200

    perf(android): enable WebView offscreen pre-rasterization + largeHeap

--- android/app/src/main/java/com/selimcelem/drift/MainActivity.java
+++ android/app/src/main/java/com/selimcelem/drift/MainActivity.java
@@ -1,5 +1,15 @@
 package com.selimcelem.drift;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = this.bridge.getWebView();
+        webView.getSettings().setOffscreenPreRaster(true);
+    }
+}
