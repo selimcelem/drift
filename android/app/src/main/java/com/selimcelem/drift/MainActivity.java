942eae8be5cd3922213260a1b798f70e7296f070
Selim Celem <selim.celem@gmail.com>
2026-04-22 23:57:54 +0200

    revert: Adreno defensive workarounds (bundle splits, largeHeap, setOffscreenPreRaster); balance: post-cap +0.10/min; docs: README screenshot gallery

--- android/app/src/main/java/com/selimcelem/drift/MainActivity.java
+++ android/app/src/main/java/com/selimcelem/drift/MainActivity.java
@@ -1,15 +1,6 @@
 package com.selimcelem.drift;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        WebView webView = this.bridge.getWebView();
-        webView.getSettings().setOffscreenPreRaster(true);
-    }
 }
